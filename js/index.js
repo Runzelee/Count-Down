dd530cc71dac7a020c7895c9ad2feb1a46a7f219
Runze Lee <lhx060906@yeah.net>
2022-05-07 11:26:46 +0800

    中考延期

--- js/index.js
+++ js/index.js
@@ -18,12 +18,12 @@ window.addEventListener("load", () => {
 	else change(1);
 
 	const getTime = () => {
-		var EndTime=new Date("2022/06/18 09:00");
+		var EndTime=new Date("2022/07/11 09:00");
 		var name="2022年中考";
 
 
 	
-		if(getQueryVariable("date")==false||getQueryVariable("time")==false||getQueryVariable("name")==false) EndTime= new Date("2022/06/18 09:00"),name="2022年上海市初中学业水平考试";
+		if(getQueryVariable("date")==false||getQueryVariable("time")==false||getQueryVariable("name")==false) EndTime= new Date("2022/07/11 09:00"),name="2022年上海市初中学业水平考试";
 		else EndTime= new Date(getQueryVariable("date")+" "+getQueryVariable("time")),name=decodeURI(getQueryVariable("name")); 
 
 		document.getElementById("name").innerHTML="距离"+decodeURI(name);
@@ -31,7 +31,7 @@ window.addEventListener("load", () => {
         var NowTime = new Date();
         var t =EndTime.getTime() - NowTime.getTime();
 		
-		if(t<0) EndTime= new Date("2022/06/18 09:00"),name="2022年上海市初中学业水平考试",t =EndTime.getTime() - NowTime.getTime();
+		if(t<0) EndTime= new Date("2022/07/11 09:00"),name="2022年上海市初中学业水平考试",t =EndTime.getTime() - NowTime.getTime();
 
         const days = Math.floor(t/1000/60/60/24);
 
